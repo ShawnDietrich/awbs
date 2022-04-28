80a47780f8c6705d772260a31d3878e3bfb497b3
Shawn Dietrich <shawn.m.dietrich@gmail.com>
2022-04-28 18:14:53 -0400

    removed react-scroll

--- src/Sections/BookNow/bookNow.js
+++ src/Sections/BookNow/bookNow.js
@@ -2,7 +2,7 @@ import React from "react";
 import doulaPic from "../../pictures/Let_s.Doula.This.png";
 import './bookNow.css';
 import { Link } from "react-router-dom";
-import {Link as scrollLink, animateScroll as scroll} from "react-scroll"
+
 
 const BookNow = () => {
   return (
