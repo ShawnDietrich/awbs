438cb535fab25d5dd0602e29aa0b97a08bd688ef
Shawn Dietrich <shawn.m.dietrich@gmail.com>
2022-08-13 11:08:11 -0400

    commented out service code

--- services/service.js
+++ services/service.js
@@ -1,3 +1,3 @@
 
-const userId = process.env.REACT_APP_CLIENTID
-const blogReqEndpont = 'https://www.googleapis.com/blogger/v3/users/userId/blogs'
+//const userId = process.env.REACT_APP_CLIENTID
+//const blogReqEndpont = 'https://www.googleapis.com/blogger/v3/users/userId/blogs'
